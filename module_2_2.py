99ea5e0585c514e8578f333e84272b6c65dfdfc0
YuriMonastyrenko <monastyrenko99@mail.ru>
2024-08-17 17:17:32 +0300

    Delete module_2_2.py

--- module_2_2.py
+++
@@ -1,9 +0,0 @@
-first = input("Введите первое число: ")
-second = input("Введите второе число: ")
-third = input("Введите третье число: ")
-if first == second == third:
-    print("Вы ввели 3 одинаковых числа.")
-elif first == second or first == third or second == third:
-    print("Вы ввели 2 одинаковых числа")
-else:
-    print("Все числа разные.")
